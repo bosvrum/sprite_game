eb874ad98e72639662e61cb99b5c31482d558885
Catalin Boruga <catalinboruga@yahoo.com>
2015-07-19 14:08:03 +0200

    Add enemy class, methods draw and move

---
+++ enemy.rb
@@ -0,0 +1,19 @@
+class Enemy
+  SPEED = 4
+  def initialize(window)
+    @radius = 20
+    @x = rand(window.width - 2 * @radius) + @radius #we set x to a random number, that will be the center of the enemy ship
+    @y = 0 # to be on top of the window
+    @image = Gosu::Image.new('images/enemy.png') #add enemy image
+  end
+
+  # it moves only in the y direction
+  def move
+    @y += SPEED
+  end
+
+  #we center the image on @x and @y using @radius
+  def draw
+    @image.draw(@x - @radius, @y - @radius, 1)
+  end
+end
--- sprite_game.rb
+++ sprite_game.rb
@@ -1,5 +1,6 @@
 require "gosu"
 require_relative 'player'
+require_relative 'enemy'
 
 class SpriteGame < Gosu::Window
   WIDTH = 800
@@ -9,10 +10,12 @@ class SpriteGame < Gosu::Window
     self.caption = 'Sprite Game'
     #in SpriteGame window is self!!
     @player = Player.new(self)
+    @enemy = Enemy.new(self)
   end
 
   def draw
     @player.draw
+    @enemy.draw
   end
 
   # we call the player methods to move the ship using gosu keys
@@ -22,6 +25,7 @@ class SpriteGame < Gosu::Window
     @player.turn_right if button_down?(Gosu::KbRight)
     @player.accelerate if button_down?(Gosu::KbUp)
     @player.move
+    @enemy.move
   end
 end
 
