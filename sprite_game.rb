ea87f9570e0e9a537d4fa608d21f76cb284d11f6
Catalin Boruga <catalinboruga@yahoo.com>
2015-07-19 12:16:11 +0200

    Add player to sprite_game, and draw method

--- sprite_game.rb
+++ sprite_game.rb
@@ -1,9 +1,16 @@
 require "gosu"
+require_relative 'player'
 
 class SpriteGame < Gosu::Window
   def initialize
     super(800, 600)
     self.caption = 'Sprite Game'
+    #in SpriteGame window is self!!
+    @player = Player.new(self)
+  end
+
+  def draw
+    @player.draw
   end
 end
 
