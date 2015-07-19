18c302a00636ac4673d56c6e77e4662b4004b109
Catalin Boruga <catalinboruga@yahoo.com>
2015-07-19 21:44:22 +0200

    Finished the game

--- sprite_game_scenes.rb
+++ sprite_game_scenes.rb
@@ -3,6 +3,8 @@ require_relative 'player'
 require_relative 'enemy' 
 require_relative 'bullet' 
 require_relative 'explosion' 
+require_relative 'credit'
+
 class SectorFive < Gosu::Window
   WIDTH = 800
   HEIGHT = 600 
@@ -14,7 +16,9 @@ class SectorFive < Gosu::Window
     @background_image = Gosu::Image.new('images/start_screen.png') 
     #here we have our new class with it's initialized method
     @scene = :start
-  end 
+    @start_music = Gosu::Song.new('sounds/Lost Frontier.ogg')
+    @start_music.play(true)
+  end
 
   def initialize_game
     @player = Player.new(self) 
@@ -25,6 +29,10 @@ class SectorFive < Gosu::Window
     #to track :how many enemies appeared and destroyed
     @enemies_appeared = 0
     @enemies_destroyed = 0
+    @game_music = Gosu::Song.new('sounds/Cephalopod.ogg')
+    @game_music.play(true)
+    @explosion_sound = Gosu::Sample.new('sounds/explosion.ogg')
+    @shooting_sound = Gosu::Sample.new('sounds/shoot.ogg')
   end
 
   def draw
@@ -100,6 +108,7 @@ class SectorFive < Gosu::Window
           @bullets.delete bullet
           @explosions.push Explosion.new(self, enemy.x, enemy.y)
           @enemies_destroyed += 1
+          @explosion_sound.play
         end
       end
     end
@@ -149,7 +158,69 @@ class SectorFive < Gosu::Window
   def button_down_game(id)
     if id == Gosu::KbSpace
       @bullets.push Bullet.new(self, @player.x, @player.y, @player.angle)
+      @shooting_sound.play(0.3)
     end
+  end
+
+  def button_down_end(id)
+    if id == Gosu::KbP 
+      initialize_game
+    elsif id == Gosu::KbQ 
+      close
+    end
+  end
+
+  def initialize_end(fate) 
+    case fate
+    when :count_reached
+      @message = "You made it!  You destroyed #{@enemies_destroyed} ships"
+      @message2= "and #{@enemies_escaped} reached the base."
+    when :hit_by_enemy
+      @message = "You were struck by an enemy ship."
+      @message2 = "Before your ship was destroyed, "
+      @message2 += "you took out #{@enemies_destroyed} enemy ships."
+    when :off_top
+      @message = "You got too close to the enemy mother ship."  
+      @message2 = "Before your ship was destroyed, "
+      @message2 += "you took out #{@enemies_destroyed} enemy ships."
+    end
+    @bottom_message = "Press P to play again, or Q to quit."
+    @message_font = Gosu::Font.new(28)
+    @credits = []
+    y = 700
+    File.open('credits.txt').each do |line|
+      @credits.push(Credit.new(self,line.chomp,100,y))
+      y+=30
+    end
+    @scene = :end
+    @end_music = Gosu::Song.new('sounds/FromHere.ogg')
+    @end_music.play(true)
+  end
+
+
+  def update_end
+    @credits.each do |credit|
+      credit.move
+    end
+    if @credits.last.y < 150
+      @credits.each do |credit|
+        credit.reset
+      end
+    end
+  end
+
+
+  def draw_end
+    clip_to(50,140,700,360) do
+      @credits.each do |credit|
+        credit.draw
+      end
+    end 
+    draw_line(0,140,Gosu::Color::RED,WIDTH,140,Gosu::Color::RED)
+    @message_font.draw(@message,40,40,1,1,1,Gosu::Color::FUCHSIA)
+    @message_font.draw(@message2,40,75,1,1,1,Gosu::Color::FUCHSIA)
+    draw_line(0,500,Gosu::Color::RED,WIDTH,500,Gosu::Color::RED)
+    @message_font.draw(@bottom_message,180,540,1,1,1,Gosu::Color::BLUE)
   end
 end
 window = SectorFive.new
